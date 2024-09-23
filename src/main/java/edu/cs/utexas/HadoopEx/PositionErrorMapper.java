0a7ee25a8ffe6baec108b350e412d72b2e7efcae
Jerry <kenhe2010@gmail.com>
2024-09-23 18:21:59 -0500

    fixed output for task 1

--- src/main/java/edu/cs/utexas/HadoopEx/PositionErrorMapper.java
+++ src/main/java/edu/cs/utexas/HadoopEx/PositionErrorMapper.java
@@ -12,7 +12,7 @@ public class PositionErrorMapper extends Mapper<Object, Text, Text, IntWritable>
 
     public void map(Object key, Text value, Context context) 
             throws IOException, InterruptedException {
-        
+
         String[] itr = value.toString().split(",");
 
         if (itr.length < 17) {
@@ -20,26 +20,41 @@ public class PositionErrorMapper extends Mapper<Object, Text, Text, IntWritable>
         }
 
         String pickupTime = itr[2];
-        int errorCount = 0;
+        String dropoffTime = itr[3];
+        int pickupErrorCount = 0;
+        int dropoffErrorCount = 0;
 
-        double pickup_longitude = Math.round(Float.parseFloat(itr[6]) * 100.0) / 100.0; 
+        double pickup_longitude = Math.round(Float.parseFloat(itr[6]) * 100.0) / 100.0;
         double pickup_latitude = Math.round(Float.parseFloat(itr[7]) * 100.0) / 100.0;
         double dropoff_longitude = Math.round(Float.parseFloat(itr[8]) * 100.0) / 100.0;
         double dropoff_latitude = Math.round(Float.parseFloat(itr[9]) * 100.0) / 100.0;
 
         if (pickup_longitude == 0 || pickup_latitude == 0) {
-             errorCount += 1;
+            pickupErrorCount += 1;
         }
 
-        if (dropoff_longitude == 0 ||dropoff_latitude == 0) {
-            errorCount += 1;
+        if (dropoff_longitude == 0 || dropoff_latitude == 0) {
+            dropoffErrorCount += 1;
         }
 
-        if (errorCount > 0) {
-            // System.out.println("*******Error coount > 0:" + errorCount + " value:" + value.toString());
+        if (pickupErrorCount > 0) {
+            // System.out.println("*******Error count > 0:" + errorCount + " value:" + value.toString());
             String hourSplit = pickupTime.split(" ")[1].split(":")[0];
+            if (hourSplit.equals("00")) {
+                hourSplit = "24";
+            }
             hour.set(hourSplit);
-            context.write(hour, new IntWritable(errorCount));
+            context.write(hour, new IntWritable(pickupErrorCount));
+        }
+
+        if (dropoffErrorCount > 0) {
+            // System.out.println("*******Error count > 0:" + errorCount + " value:" + value.toString());
+            String hourSplit = dropoffTime.split(" ")[1].split(":")[0];
+            if (hourSplit.equals("00")) {
+                hourSplit = "24";
+            }
+            hour.set(hourSplit);
+            context.write(hour, new IntWritable(dropoffErrorCount));
         }
     }
 }
--- src/main/java/edu/cs/utexas/HadoopEx/PositionErrorReducer.java
+++ src/main/java/edu/cs/utexas/HadoopEx/PositionErrorReducer.java
@@ -1,9 +1,10 @@
 package edu.cs.utexas.HadoopEx;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.TreeMap;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -14,15 +15,24 @@ import org.apache.log4j.Logger;
 public class PositionErrorReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 
     private Logger logger = Logger.getLogger(PositionErrorReducer.class);
-    
-    public void reduce(Text key, Iterable<IntWritable> values, Context context) 
+    private Map<String, IntWritable> map = new TreeMap<String, IntWritable>();;
+
+    public void reduce(Text key, Iterable<IntWritable> values, Context context)
             throws IOException, InterruptedException {
         int errorCount = 0;
-        
+
         for (IntWritable value : values) {
             errorCount += value.get();
         }
-        
-        context.write(key, new IntWritable(errorCount)); 
+
+        map.put(key.toString(), new IntWritable(errorCount));
+        context.write(key, new IntWritable(errorCount));
     }
+
+    public void cleanup(Context context) throws IOException, InterruptedException {
+        for (Map.Entry<String, IntWritable> entry : map.entrySet()) {
+            logger.info("(" + (Integer.parseInt(entry.getKey())) + ", " + entry.getValue() + ")");
+        }
+    }
+    
 }
